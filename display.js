45bf3194c3be33b22c992c11b3534d2ec944b412
Aditya Basumatary <45668657+Adi9957@users.noreply.github.com>
2022-03-29 00:30:20 +0530

    Hellow world

--- display.js
+++ display.js
@@ -29,7 +29,6 @@ const res = require("express/lib/response");
 
   display.use(express.static('views')); 
 display.use('/image', express.static('image'));
-
   display.get('/', function(req,res){
 
 
@@ -39,7 +38,7 @@ display.use('/image', express.static('image'));
     db.collection('newsdata').find({  }).toArray(function(err, docs){
       if (docs) {
           res.render("index3", {
-              data: docs
+              datalst: docs
           });
           console.log(docs)
           console.log(docs.length)
@@ -48,4 +47,256 @@ display.use('/image', express.static('image'));
           console.log('Failed to retrieve the Course List: ' + err);
       }
   });
-  }).listen(8111)
+
+
+  })
+
+  
+
+  
+//Register For Admin. /////////////////////
+
+
+  
+  
+display.use(bodyParser.json());
+display.use(express.static('public'));
+display.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+let alert = require('alert'); 
+
+
+
+
+//Register For Admin.
+
+display.post('/sign_up', function(req,res){
+
+  
+  var namevalue =req.body.name;
+  var emailvalue =req.body.email;
+  var passvalue = req.body.password;
+  
+
+
+  var data = {
+      "name": namevalue,
+      "email":emailvalue,
+      "password":passvalue,
+      
+     
+      
+  }
+ 
+
+  
+db.collection('Admin').findOne({ email: emailvalue }, function(err, user)
+   { 
+     
+
+
+    
+      if (user) 
+    
+      { 
+      
+        alert("Your Email Is Already Exists")
+        console.log("Your Email Is Already Exists") 
+      }
+      else
+      { 
+        db.collection('Admin').insertOne(data,function(err, result){
+           if (err) throw err;
+           alert("Register Successfully")
+               console.log("Register Successfully");
+            
+        });
+        
+        return res.sendFile(__dirname + "/loginform.html");
+        
+      } 
+
+
+    })
+
+
+ 
+
+
+
+   
+        
+           
+
+    
+    
+    
+   
+        
+
+
+
+
+  
+
+
+})
+
+  
+
+
+  
+
+
+//Login For Admin.
+
+display.post('/login', function(req,res){
+
+  
+  var emailvalue =req.body.email;
+  var passvalue = req.body.password;
+
+
+  var data = {
+      "email":emailvalue,
+      "password":passvalue,
+      
+     
+      
+  }
+  
+
+
+
+  
+  db.collection('Admin').findOne({ email: emailvalue,password:passvalue }, function(err, user)
+  { 
+    
+
+
+   
+     if (user) 
+   
+      { 
+
+        alert("Login Successfully")
+             res.sendFile(__dirname + "/uploadform.html")
+
+            //Uploading process here 
+            
+            display.post('/upload_data', function(req,res){
+
+              
+              var titleheading =req.body.Field1;
+              var titledescription =req.body.Field3;
+
+              var contentdescreadless =req.body.Field4;
+              var contentdescreadmore =req.body.Field5;
+
+
+
+            
+            
+                  
+                
+                  
+            
+            
+
+              
+                
+                  db.collection('newsdata').insertOne({ "theading": titleheading ,"tdes":titledescription,"cdesrl":contentdescreadless,"cdesrm":contentdescreadmore }, function(err, user){   
+                  if (user) 
+                
+                  { 
+                  
+                    alert("Uploaded Successfully")
+                    console.log("Uploaded Successfully") 
+                    
+
+                      
+
+
+
+    
+                     
+                  
+                  }
+                  else
+                  { 
+                    
+                      alert("Failed To Upload")
+                          console.log("Failed To Upload");
+                        
+                  
+                    
+            
+                    
+                  } 
+
+
+              })
+
+            })
+
+            ///Till here upload process is done;////////////////////////////////////////
+
+
+            //////////////////Now data edit process start/////////////////////////////////////////
+
+
+
+
+
+            display.post('/edit_data', function(req,res){
+              alert("Trying To Edit")
+                          console.log("Trying To Edit");
+
+
+            })
+      }
+     else
+        { 
+
+
+          alert("You Don't Seems To  Have A Account Here. Please Create A New Acoount")
+            console.log("You Don't Seems To  Have A Account Here. Please Create A New Acoount");
+      
+       
+        } 
+
+
+   })
+        
+  
+})
+
+
+
+
+
+  
+// By Clicking On the link Sign in .href you can go to loginform.html
+display.get('/login',function(req,res) {
+  res.sendFile(__dirname + "/loginform.html")});
+
+
+  // By Clicking On the link Sign in .href you can go to registerfor.html
+display.get('/register',function(req,res) {
+  res.sendFile(__dirname + "/register.html")});
+
+
+
+
+
+//This the first page where admin will  sign up by default page
+display.get('/admin',function(req,res){
+   res.sendFile(__dirname + "/loginform.html")
+
+
+}).listen(4221)
+
+
+console.log("server listening at port 8080");
